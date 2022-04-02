32842a4089f3572b473b24a718f07d2fceec8822
Unskilled Wolf <geronimo.xl13@gmail.com>
2022-04-02 15:05:56 +0200

    Block Script

--- src/scripts/Block.gd
+++ src/scripts/Block.gd
@@ -1,4 +1,30 @@
+class_name Block
 extends StaticBody2D
 
-func _ready():
-	pass # Replace with function body.
+onready var timer = $Timer
+onready var collider = $CollisionShape2D
+
+var target_scale = Vector2.ZERO;
+var start_scale = Vector2.ZERO;
+var target_collision = false;
+var is_changing = false;
+
+func set_block_scale(scale_to: Vector2, speed: float, collision_on_finish: bool):
+	print("["+name+"] Going to scale " + str(scale_to))
+
+	target_scale = scale_to
+	start_scale = scale
+	target_collision = collision_on_finish
+
+	is_changing = true
+	timer.start(speed)
+
+func _process(_delta):
+	if is_changing:
+		scale = lerp(target_scale, start_scale, timer.time_left/timer.wait_time)
+
+func _on_Timer_timeout():
+	print("["+name+"] Timeout")
+	is_changing = false
+	scale = target_scale
+	collider.disabled = !target_collision
--- src/scripts/Main_Scene.gd
+++ src/scripts/Main_Scene.gd
@@ -1,26 +1,35 @@
 extends Node2D
 
-export (int) var grid_size = 10
+export (int) var grid_size = 10 # Everything proboaply breaks if this changes
 
 onready var play_area = $PlayArea
 onready var block_scene = preload("res://src/scenes/Block.tscn")
 
+var block_size = Vector2.ZERO
+
+var blocks = []
+
 func spawn_blocks():
 	print("[Main Scene] Instancing blocks...")
 
-	var block_size = play_area.scale / grid_size * 2
-	var offset = play_area.scale * block_size + Vector2.ONE*7
+	block_size = play_area.scale / grid_size * 2
+	var offset = play_area.scale * block_size + Vector2.ONE*7 # Make this work better
 
 	for y in range(grid_size):
+		var tmp = []
 		for x in range(grid_size):
 			var instance = block_scene.instance()
-			instance.scale = block_size
+			instance.scale = Vector2.ZERO
 			instance.position = Vector2(
 				8.0*x*block_size.x,
 				8.0*y*block_size.y
 			) - offset
-			instance.modulate = Color(float(x)/grid_size,float(y)/grid_size, 0)
-			add_child(instance) 
+			# instance.modulate = Color(float(x)/grid_size,float(y)/grid_size, 0)
+
+			tmp.append(instance)
+			add_child(instance)
+		blocks.append(tmp)
 
 func _ready():
 	spawn_blocks()
+	blocks[9][9].set_block_scale(block_size, 5, true)
